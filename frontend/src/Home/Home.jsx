36ce1db0f3e8a651344aeaa058bb2da455186b70
rachana-gandham <162718487+rachana-gandham@users.noreply.github.com>
2025-03-20 10:43:13 +0530

    Update Home.jsx

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -657,6 +657,7 @@ export default function Home() {
                         640: { slidesPerView: 1 },
                         768: { slidesPerView: 2 },
                         1024: { slidesPerView: 3 },
+                       1280: { slidesPerView: 4 },
                     }}
                     className="books-swiper"
                 >
